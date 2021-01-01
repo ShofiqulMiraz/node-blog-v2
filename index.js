8eef75acca09721406cd78c9bda6ce08b6ef95de
Shofiqul Islam Miraz <47095132+ShofiqulMiraz@users.noreply.github.com>
2021-01-01 22:08:03 +0600

    added hpp for preventing parameter pollutiin

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 var cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+var xss = require("xss-clean");
+var hpp = require("hpp");
 
 // middlewares
 // helmet middleware
@@ -17,16 +20,24 @@ app.use(helmet());
 
 // rate limiting
 const apiLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 15 minutes
+  windowMs: 60 * 60 * 1000, // 60 minutes
   max: 100,
   message: "Too Many Request, Please try again after 1 hour",
 });
-
 app.use("/api/", apiLimiter);
 
 app.use(express.json({ limit: "10kb" }));
 app.use(morgan("dev"));
 app.use(cors());
+
+// express-mongo-sanitize
+app.use(mongoSanitize());
+
+// XSS clean
+app.use(xss());
+
+// hpp for prevent parameter pollution
+app.use(hpp());
 
 // DB connect
 mongoose
